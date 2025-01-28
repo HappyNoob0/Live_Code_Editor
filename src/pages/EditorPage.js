8506c7b5476dc1c4701de40b3dda246703b786bd
Aditya Ranjan <adityaranjan111104@gmail.com>
2025-01-28 20:33:58 +0530

    Editor Page Setup Part-1

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, {useState} from 'react';
+import Client from '../components/Client';
 
 const EditorPage = () => {
-  return <div>Editor Page</div>;
+
+  const [clients, setClients] = useState([
+    { socketId: 1, username: 'aditya'},
+    { socketId: 2, username: 'piyush'},
+  ]);
+    
+  return (
+  
+    <div className="mainWrap">
+
+    <div className="aside">
+        <div className="asideInner">
+            <div className="logo">
+              <img className="logoImage" src="/code-sync.png" alt="logo"></img>
+            </div>
+            <h3>Connected</h3>
+            <div className="clientList">
+              {clients.map((client) => (
+                  <Client 
+                  key={client.socketId}
+                  username={client.username} 
+                  />
+                ))}
+
+            </div>
+        </div>
+    </div>
+    <div className="editorWrap">Editor goes here</div>
+
+  </div>
+  
+  );
 };
 
 export default EditorPage;
