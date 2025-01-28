7fd001b0c2851c36ef1cc51b77c5ae82060a5bba
Piyush Kumar <154805249+Piyush22536@users.noreply.github.com>
2025-01-28 18:36:21 +0530

    Unique Id Setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
-import React from "react";
-
+import React , {useState} from "react";
+import {v4 as uuidV4} from 'uuid';
 const Home = () => {
+  
+  const [roomId,setRoomId] = useState('');
+  const [username,setUsername] = useState('');
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+  }
   return (
     <div className="homePageWrapper">
         <div className="formWrapper">
@@ -14,16 +23,20 @@ const Home = () => {
                   type="text" 
                   className="inputBox" 
                   placeholder="ROOM ID" 
+                  onChange = {(e) => setRoomId(e.target.value)}
+                  value = {roomId}
                 />
                <input 
                   type="text" 
                   className="inputBox" 
                   placeholder="USER NAME"
+                  onChange = {(e) => setUsername(e.target.value)}
+                  value = {username}
                />
           <button className="btn joinBtn">join</button>
           <span className="createInfo">
                If you don't have an invite then create &nbsp;
-              <a 
+              <a onClick = {createNewRoom}
                   href="" className="createNewBtn"
                   >
                     new room
