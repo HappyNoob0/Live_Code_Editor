dc2874ce5dd88e260f27d5aee40aab58e7c510a7
Aditya Ranjan <adityaranjan111104@gmail.com>
2025-02-01 15:01:16 +0530

    Copy Room Id and Leave Functionality

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -53,6 +53,20 @@ const EditorPage = () => {
       socketRef.current.off(ACTIONS.DISCONNECTED);
     };
  },[]);
+
+ async function copyRoomId() {
+  try {
+    await navigator.clipboard.writeText(roomId);
+    toast.success('Room ID has been copied to your clipboard');
+  } catch(err) {
+    toast.error('Could not copy the Room ID');
+    console.error(err);
+  }
+ }
+
+ function leaveRoom() {
+  reactNavigator('/');
+ }
   
   if(!location.state){
     return   <Navigate  to ="/" />;
@@ -74,8 +88,8 @@ const EditorPage = () => {
           </div>
         </div>
 
-        <button className="btn copyBtn">Copy ROOM ID</button>
-        <button className="btn leaveBtn">Leave</button>
+        <button className="btn copyBtn" onClick={copyRoomId}>Copy ROOM ID</button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>Leave</button>
       </div>
 
       <div className="editorWrap">
